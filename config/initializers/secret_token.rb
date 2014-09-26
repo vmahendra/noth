c93d76c4ade4120187ac5376125f2b560b2b874b
vmahendra <vijaymahendra85@gmail.com>
2014-09-26 17:58:49 +0530

    rotating img

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noth::Application.config.secret_token = '7965e9c6ab3a74d4f250c83ac53637df3424e1f5328a77eaa8eef042534ab74f7a12470395e6b6445b21232aa956ce7d03cf09749247779753de1106e04a3fdd'
